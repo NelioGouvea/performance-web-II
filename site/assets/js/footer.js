0c601f6ba8cbef0dbdd062153d9b3d8fb0f75c37
Nélio <nelioalvesgouvea@gmail.com>
2023-09-25 19:56:44 -0300

    Finaliza capítulo 8 - Resource Hints

--- site/assets/js/footer.js
+++ site/assets/js/footer.js
@@ -7,6 +7,13 @@ setTimeout( function() {
 		var inputEmail = document.querySelector('.footer-newsletter-input');
 		isValid = validaEmail(inputEmail.value);
 
+		inputEmail.onfocus = function() {
+			var prerender = document.createElement('link');
+			prerender.rel = 'prerender';
+			prerender.href = 'cadastrado.html';
+			document.head.appendChild(prerender);
+		}
+
 		if(!isValid) {
 			inputEmail.classList.add('form-erro');
 			document.querySelector('.footer-newsletter-button').classList.add('form-erro');
